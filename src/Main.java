4681f6123c5a4af3ed2040f6467495c4b0270591
Damien Beltran <119611108+Damienb123@users.noreply.github.com>
2024-05-02 19:52:32 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 
 
-
+/**
+ * Main class to demonstrate the usage of MathOperation class.
+ */
 public class Main {
+
+	/**
+	 * Entry point of the program.
+	 * Initialize a MathOperation object and calculates the factorial of 5.
+	 * @param args
+	 */
 	
 	public static void main(String[] args) {
-		MathOperation mathOperation = new MathOperation(); //mathOperation local variable intialized
-		int factorialofFive = mathOperation.factorial(5);
-		System.out.println("Factorial of 5 is: " + factorialofFive);
-		
-		
-		
+		MathOperation mathOperation = new MathOperation(); //Initialize the MathOperation object
+		int factorialofFive = mathOperation.factorial(5); //Calculate the factorial of 5
+		System.out.println("Factorial of 5 is: " + factorialofFive); //Print the result
 	}
 
 }
